0ff6739252d214b63ceea7990e2b3bcdcbe2a467
Alexandre Senges <alexandre@senges.ch>
2019-09-20 12:51:27 +0200

    Ex1

--- week1/ex1.r
+++ week1/ex1.r
@@ -2,7 +2,7 @@ random_average = function(count) {
     mu = c()
 
     for(i in 1:1000) {
-        x = rnorm(1000, -.3, 1.2)
+        x = rnorm(count, -0.3, 1.2)
         s = mean(x)
         mu = c(mu, s)
     }
@@ -10,14 +10,11 @@ random_average = function(count) {
     return(mu)
 }
 
-
 plot_random_densities = function(){
-    output = c()
     cond = FALSE
     for(i in c(30, 100, 1000, 10000)){
         randomized_average = random_average(i)
         dens = density(randomized_average)
-        #deviation = sd(random_average)
         #cbind(dens, deviation)
         if(!cond){
             plot(dens)
---
+++ week1/ex1_2.r
@@ -0,0 +1,11 @@
+f_x = function(x) {
+    return(1/sqrt(x))
+}
+
+
+main = function(){
+    plot(f_x)
+}
+
+
+main()
