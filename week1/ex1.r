dd7605ef63c891e6f25777513a1ce11422466219
Alexandre Senges <alexandre@senges.ch>
2019-09-20 12:05:09 +0200

    random_averages, plot

--- week1/ex1.r
+++ week1/ex1.r
@@ -1,9 +1,37 @@
-mu = c()
+random_average = function(count) {
+    mu = c()
 
-for(i in 1:1000) {
-    x = rnorm(30, -.3, 1.2)
-    s = mean(x)
-    mu=c(mu, s)
+    for(i in 1:1000) {
+        x = rnorm(1000, -.3, 1.2)
+        s = mean(x)
+        mu = c(mu, s)
+    }
+
+    return(mu)
 }
 
-print(mu)
+
+plot_random_densities = function(){
+    output = c()
+    cond = FALSE
+    for(i in c(30, 100, 1000, 10000)){
+        randomized_average = random_average(i)
+        dens = density(randomized_average)
+        deviation = sd(random_average)
+        #cbind(dens, deviation)
+        if(!cond){
+            plot(dens)
+            cond = TRUE
+        }
+        else{
+            lines(dens)
+        }
+    }
+}
+
+main = function(){
+    plot_random_densities()
+}
+
+
+main()
